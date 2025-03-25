d91ac4d65f829a2ac73171e21e5c0f353e57073b
Thedas Silva <thedassilva64@gmail.com>
2025-03-25 18:09:26 +0530

    hi

--- backend/models/vendor.js
+++ backend/models/vendor.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const vendorSchema = new mongoose.Schema({
   vendorName: { type: String, required: true },
   contactPerson: { type: String, required: true },
